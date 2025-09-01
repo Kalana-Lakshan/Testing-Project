3426dcf3a8166cb16f5fc83051cc4b612bd2d7f2
KRakeesh04 <kanesrakeshan@gmail.com>
2025-09-01 21:29:08 +0530

    chore: add tokeniser functionality, hashing functions and role guard for routes

---
+++ backend/src/auth/auth.ts
@@ -0,0 +1,23 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export interface AuthRequest extends Request {
+	user?: {
+		roles: string[];
+	};
+}
+
+function authorizeRoles(allowedRoles: string[]) {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Unauthorized: No user info' });
+    }
+
+    const hasRole = req.user.roles.some(role => allowedRoles.includes(role));
+    if (!hasRole) {
+      return res.status(403).json({ message: 'Forbidden: Insufficient role' });
+    }
+    next();
+  };
+}
+
+export default authorizeRoles;
---
+++ backend/src/auth/token.ts
@@ -0,0 +1,37 @@
+import jwt  from 'jsonwebtoken';
+import type {Secret, SignOptions, JwtPayload} from 'jsonwebtoken';
+
+
+const JWT_SECRET: Secret = process.env.JWT_SECRET || "your_jwt_secret";
+const JWT_EXPIRATION: SignOptions = { expiresIn: (process.env.JWT_EXPIRATION as any) || "1h" };
+
+interface UserPayload {
+  userId: string;
+  role: string;
+}
+
+export function generateToken(user: UserPayload): string {
+  const payload = {
+    userId: user.userId,
+    role: user.role,
+  };
+  return jwt.sign(payload, JWT_SECRET, JWT_EXPIRATION);
+}
+
+
+export function authenticateToken(token: string): UserPayload | null {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+    if (decoded && typeof decoded === 'object' && 'userId' in decoded && 'role' in decoded) {
+      return {
+        userId: decoded.userId as string,
+        role: decoded.role as string
+      };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    return null;
+  }
+}
