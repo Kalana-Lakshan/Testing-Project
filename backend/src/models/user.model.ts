27b4ed5783ba7286fb493a384498252beb1d9d9d
KRakeesh04 <kanesrakeshan@gmail.com>
2025-09-05 19:31:14 +0530

    chore: add user type in backend

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,15 @@
 import sql from "../db.js";
 
+export interface User {
+  user_id       : number;
+  username      : string;
+  password_hash : string;
+  role          : string;
+  branch_id     : number;
+  is_approved   : boolean;
+  created_at    : Date;
+}
+
 export const getUserById = async (id: number) => {
   const [user] = await sql`SELECT * FROM users WHERE id = ${id}`;
   return user;
