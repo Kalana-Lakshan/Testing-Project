a07966f75f55f10ad9fe84c6b183c2ad8333790a
KRakeesh04 <kanesrakeshan@gmail.com>
2025-09-06 03:17:23 +0530

    chore: add procedures for branch and user_contact models

--- database/procedures.sql
+++ database/procedures.sql
@@ -3,6 +3,8 @@
 DROP PROCEDURE IF EXISTS create_user(VARCHAR, VARCHAR, VARCHAR, BIGINT, BOOLEAN);
 DROP PROCEDURE IF EXISTS update_user(BIGINT, VARCHAR, VARCHAR, VARCHAR, BIGINT, BOOLEAN);
 DROP FUNCTION IF EXISTS get_user_by_id(BIGINT);
+DROP FUNCTION IF EXISTS get_user_by_username(VARCHAR);
+DROP FUNCTION IF EXISTS get_all_users(INT, INT);
 DROP PROCEDURE IF EXISTS delete_user(BIGINT);
 -- Patient model functions
 -- Drop existing objects (if they exist)
@@ -10,6 +12,7 @@ DROP PROCEDURE IF EXISTS create_patient(BIGINT, VARCHAR, VARCHAR, VARCHAR, VARCH
 DROP PROCEDURE IF EXISTS update_patient(BIGINT, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, DATE, VARCHAR);
 DROP FUNCTION IF EXISTS get_patient_by_id(BIGINT);
 DROP FUNCTION IF EXISTS get_patients_by_blood_type(VARCHAR);
+DROP FUNCTION IF EXISTS get_all_patients(INT, INT);
 DROP PROCEDURE IF EXISTS delete_patient(BIGINT);
 -- Staff model functions
 -- Drop existing objects (if they exist)
@@ -17,13 +20,28 @@ DROP PROCEDURE IF EXISTS create_staff(BIGINT, VARCHAR, VARCHAR, VARCHAR, NUMERIC
 DROP PROCEDURE IF EXISTS update_staff(BIGINT, VARCHAR, VARCHAR, VARCHAR, NUMERIC);
 DROP FUNCTION IF EXISTS get_staff_by_id(BIGINT);
 DROP FUNCTION IF EXISTS get_staffs_by_type(VARCHAR);
+DROP FUNCTION IF EXISTS get_staffs_by_branch_id(BIGINT);
 DROP PROCEDURE IF EXISTS delete_staff(BIGINT);
 -- Branch Manager model functions
 -- Drop existing objects (if they exist)
 DROP PROCEDURE IF EXISTS create_branch_manager(BIGINT, VARCHAR, NUMERIC, VARCHAR);
 DROP PROCEDURE IF EXISTS update_branch_manager(BIGINT, VARCHAR, NUMERIC, VARCHAR);
 DROP FUNCTION IF EXISTS get_branch_manager_by_id(BIGINT);
+DROP FUNCTION IF EXISTS get_branch_manager_by_branch_id(BIGINT);
 DROP PROCEDURE IF EXISTS delete_branch_manager(BIGINT);
+-- Branch model functions
+-- Drop existing objects (if they exist)
+DROP PROCEDURE IF EXISTS create_branch(VARCHAR, VARCHAR, VARCHAR);
+DROP PROCEDURE IF EXISTS update_branch(BIGINT, VARCHAR, VARCHAR, VARCHAR);
+DROP FUNCTION IF EXISTS get_branch_by_id(BIGINT);
+DROP PROCEDURE IF EXISTS delete_branch(BIGINT);
+-- User_Contact model functions
+-- Drop existing objects (if they exist)
+DROP PROCEDURE IF EXISTS create_user_contact(VARCHAR, VARCHAR, BOOLEAN, BIGINT);
+DROP PROCEDURE IF EXISTS update_user_contact(VARCHAR, VARCHAR, BOOLEAN, BIGINT);
+DROP FUNCTION IF EXISTS get_contact(VARCHAR);
+DROP FUNCTION IF EXISTS get_default_contacts_by_userID(BIGINT);
+DROP PROCEDURE IF EXISTS delete_branch(BIGINT);
 
 
 
@@ -86,7 +104,52 @@ BEGIN
 END;
 $$;
 
--- 4. Delete a user 
+-- 3. Get a user by username 
+CREATE OR REPLACE FUNCTION get_user_by_username(p_username VARCHAR)
+RETURNS TABLE(
+  user_id      BIGINT,
+  username     VARCHAR,
+  password_hash VARCHAR,
+  role         VARCHAR,
+  branch_id    BIGINT,
+  is_approved  BOOLEAN,
+	created_at	TIMESTAMP
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT user_id, username, password_hash, role, branch_id, is_approved
+  FROM "User"
+  WHERE username = p_username;
+END;
+$$;
+
+-- 5. Get all user 
+CREATE OR REPLACE FUNCTION get_all_users(
+	user_count INT, 
+	start_count INT
+	)
+RETURNS TABLE(
+  user_id      BIGINT,
+  username     VARCHAR,
+  password_hash VARCHAR,
+  role         VARCHAR,
+  branch_id    BIGINT,
+  is_approved  BOOLEAN,
+	created_at	TIMESTAMP
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT user_id, username, password_hash, role, branch_id, is_approved, created_at
+  FROM "User"
+	ORDER BY user_id
+	LIMIT user_count
+	OFFSET start_count;
+END;
+$$;
+
+-- 6. Delete a user 
 CREATE OR REPLACE PROCEDURE delete_user(p_id BIGINT)
 LANGUAGE plpgsql
 AS $$
@@ -183,7 +246,33 @@ BEGIN
 END;
 $$;
 
--- 5. Delete a patient 
+-- 5. Get all patient 
+CREATE OR REPLACE FUNCTION get_all_patients(
+	patient_count INT,
+	count_start INT
+	)
+RETURNS TABLE(
+  patient_id  BIGINT,
+  name        VARCHAR,
+  gender      VARCHAR,
+  emergency_contact_no  VARCHAR,
+	nic         varchar,
+  address     VARCHAR,
+  date_of_birth DATE,
+  blood_type  VARCHAR
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT patient_id, name, gender, emergency_contact_no, nic, address, date_of_birth, blood_type
+  FROM "Patient"
+  ORDER BY patient_id
+	LIMIT patient_count
+	OFFSET count_start;
+END;
+$$;
+
+-- 6. Delete a patient 
 CREATE OR REPLACE PROCEDURE delete_patient(p_id BIGINT)
 LANGUAGE plpgsql
 AS $$
@@ -266,7 +355,27 @@ BEGIN
 END;
 $$;
 
--- 5. Delete a staff 
+-- 5. Get a staffs by branch id 
+CREATE OR REPLACE FUNCTION get_staffs_by_branch_id(p_branch_id BIGINT)
+RETURNS TABLE(
+	staff_id BIGINT,
+	name VARCHAR,
+	type VARCHAR,
+	gender VARCHAR,
+	monthly_salary NUMERIC
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT s.staff_id, s.name, s.type, s.gender, s.monthly_salary
+  FROM "Staff" s
+	LEFT OUTER JOIN "User" u
+	ON s.staff_id = u.user_id
+  WHERE u.branch_id = p_branch_id;
+END;
+$$;
+
+-- 6. Delete a staff 
 CREATE OR REPLACE PROCEDURE delete_staff(p_id BIGINT)
 LANGUAGE plpgsql
 AS $$
@@ -327,7 +436,26 @@ BEGIN
 END;
 $$;
 
--- 4. Delete a branch manager 
+-- 3. Get a branch manager by branch ID 
+CREATE OR REPLACE FUNCTION get_branch_manager_by_branch_id(p_branch_id BIGINT)
+RETURNS TABLE(
+	manager_id BIGINT,
+	name varchar,
+	monthly_salary NUMERIC,
+	gender varchar
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT manager_id, name, monthly_salary, gender
+  FROM "Branch_Manager" b
+	LEFT OUTER JOIN  "User" u
+	ON u.user_id = b.manager_id
+  WHERE u.branch_id = p_branch_id;
+END;
+$$;
+
+-- 5. Delete a branch manager 
 CREATE OR REPLACE PROCEDURE delete_branch_manager(p_id BIGINT)
 LANGUAGE plpgsql
 AS $$
@@ -335,6 +463,150 @@ BEGIN
   DELETE FROM "Branch_Manager" WHERE manager_id = p_id;
 END;
 $$;
+
+
+-- branch model functions
+-- 1. Create a branch  
+CREATE OR REPLACE PROCEDURE create_branch(
+	p_name varchar,
+	p_location varchar,
+	p_landline_no varchar
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  INSERT INTO "Branch" (name, location, landline_no, created_at)
+  VALUES (p_name, p_location, p_landline_no, NOW());
+END;
+$$;
+
+-- 2. update a branch 
+CREATE OR REPLACE PROCEDURE update_branch(
+  p_branch_id BIGINT,
+	p_name VARCHAR,
+	p_location VARCHAR,
+	p_landline_no VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  UPDATE "Branch"
+  SET name = p_name,
+			location = p_location,
+			landline_no = p_landline_no
+  WHERE id = p_branch_id;
+END;
+$$;
+
+-- 3. Get a branch by ID 
+CREATE OR REPLACE FUNCTION get_branch_by_id(p_branch_id BIGINT)
+RETURNS TABLE(
+	id BIGINT,
+	name VARCHAR,
+	location VARCHAR,
+	landline_no VARCHAR,
+	created_at TIMESTAMP
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT id, name, location, landline_no, created_at
+  FROM "Branch"
+  WHERE id = p_branch_id;
+END;
+$$;
+
+-- 4. Delete a branch 
+CREATE OR REPLACE PROCEDURE delete_branch(p_id BIGINT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  DELETE FROM "Branch" WHERE id = p_id;
+END;
+$$;
+
+-- user contact model functions
+-- 1. Create a user contact  
+CREATE OR REPLACE PROCEDURE create_user_contact(
+	p_contact VARCHAR,
+	p_contact_type VARCHAR,
+	p_is_default BOOLEAN,
+	p_user_id BIGINT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  INSERT INTO "User_Contact" (contact, contact_type, is_default, user_id)
+  VALUES (p_contact, p_contact_type, p_is_default, p_user_id);
+END;
+$$;
+
+-- 2. update a user contact 
+CREATE OR REPLACE PROCEDURE update_user_contact(
+  p_contact VARCHAR,
+	p_contact_type VARCHAR,
+	p_is_default BOOLEAN,
+	p_user_id BIGINT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  UPDATE "User_Contact"
+  SET contact = p_contact,
+			contact_type = p_contact_type,
+			is_default = p_is_default,
+			user_id = p_user_id
+  WHERE contact = p_contact;
+END;
+$$;
+
+-- 3. Get a user contact by contact
+CREATE OR REPLACE FUNCTION get_contact(p_contact VARCHAR)
+RETURNS TABLE(
+	contact VARCHAR,
+	contact_type VARCHAR,
+	is_default BOOLEAN,
+	user_id BIGINT
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT contact, contact_type, is_default, user_id
+  FROM "User_Contact"
+  WHERE contact = p_contact;
+END;
+$$;
+
+-- 4. Get default user contact by user id
+CREATE OR REPLACE FUNCTION get_default_contacts_by_userID(p_userID BIGINT)
+RETURNS TABLE(
+	contact VARCHAR,
+	contact_type VARCHAR,
+	is_default BOOLEAN,
+	user_id BIGINT
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT contact, contact_type, is_default, user_id
+  FROM "User_Contact"
+  WHERE user_id = p_userID ADD is_default = TRUE;
+END;
+$$;
+
+-- 5. Delete a user contact 
+CREATE OR REPLACE PROCEDURE delete_contact(p_contact VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  DELETE FROM "User_Contact" WHERE contact = p_contact;
+END;
+$$;
+
+
+
+
+
 
 
 
