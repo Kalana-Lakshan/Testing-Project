4d38b6c87914094bcd6c25532880e715a5f3a556
KRakeesh04 <kanesrakeshan@gmail.com>
2025-09-17 03:15:54 +0530

    chore: add some sample frontend router paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+// import './App.css'
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from 'react-router-dom';
+import PageTitle from './components/PageTitle';
+import DefaultLayout from './DefaultLayout';
+import Dashboard from './pages/Dashboard';
+import { Skeleton } from "./components/ui/skeleton";
+import SignIn from "./pages/Authentication/sign-in";
+import SignUp from "./pages/Authentication/sign-up";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(false);
+	const { pathname } = useLocation();
 
-  return (
+  useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+  return loading ? (
+		<div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[150px]" />
+      </div>
+    </div>
+	) : (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+            <Route
+              path="/sign-in"
+              element={
+                <>
+                  <PageTitle title="Sign-in | MedSync" />
+                  <SignIn />
+                </>
+              }
+            />
+
+            <Route
+              path="/sign-up"
+              element={
+                <>
+                  <PageTitle title="Sign-up | MedSync" />
+                  {/* <SignUp /> */}
+                </>
+              }
+            />
+
+            <Route element={<DefaultLayout />}>
+              <Route
+                index
+                element={
+                  <>
+                    <PageTitle title="Dashboard | MedSync" />
+                    <Dashboard />
+                  </>
+                }
+              />
+
+              <Route
+              path=""
+              element={
+                <>
+                  {/* <PageTitle title="" /> */}
+                  {/* < /> */}
+                </>
+              }
+            />
+
+            </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
