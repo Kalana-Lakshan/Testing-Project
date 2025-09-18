c423564fe7e07e9ce04edf2e1643142f88149143
klacc2022 <thilakarathnapemalatha@gmail.com>
2025-09-18 15:49:25 +0530

    added mysql code according to ERD

--- backend/src/db/table.sql
+++ backend/src/db/table.sql
@@ -1,122 +1,239 @@
-USE `Project-MedSync`;
-
--- child → parent drop order
-DROP TABLE IF EXISTS `billing_payment`;
-DROP TABLE IF EXISTS `billing_invoice`;
-DROP TABLE IF EXISTS `prescription`;
-DROP TABLE IF EXISTS `treatment`;
-DROP TABLE IF EXISTS `medical_history`;
-DROP TABLE IF EXISTS `insurance_claim`;
-DROP TABLE IF EXISTS `patient_insurance`;
-DROP TABLE IF EXISTS `doctor_speciality`;
-DROP TABLE IF EXISTS `patient`;
-DROP TABLE IF EXISTS `doctor`;
-DROP TABLE IF EXISTS `branch_manager`;
-DROP TABLE IF EXISTS `staff`;
-DROP TABLE IF EXISTS `user_contact`;
-DROP TABLE IF EXISTS `log`;
-DROP TABLE IF EXISTS `action`;
-DROP TABLE IF EXISTS `appointment`;
-DROP TABLE IF EXISTS `treatment_catelogue`;
-DROP TABLE IF EXISTS `speciality`;
-DROP TABLE IF EXISTS `insurance`;
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `branch`;
-
-
-
-CREATE TABLE `branch` (
-    `branch_id` BIGINT AUTO_INCREMENT,
-    `name` VARCHAR(15) NOT NULL UNIQUE,
-    `location` VARCHAR(100) NOT NULL,
-    `landline_no` VARCHAR(12) NOT NULL UNIQUE,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`branch_id`)
+CREATE TABLE `Speciality` (
+  `speciality_id` int,
+  `speciality_name` varchar(20),
+  `description` varchar(255),
+  PRIMARY KEY (`speciality_id`)
 );
 
-
-CREATE TABLE `user` (
-    `user_id` BIGINT AUTO_INCREMENT,
-    `username` VARCHAR(20) NOT NULL UNIQUE,
-    `password_hash` VARCHAR(50) NOT  NULL,
-    `role` ENUM(
-        'Super_Admin', 
-        'Branch_Manager', 
-        'Doctor', 
-        'Admin_Staff', 
-        'Nurse', 
-        'Receptionist', 
-        'Billing_Staff', 
-        'Insurance_Agent', 
-        'Patient'
-    ) NOT NULL,
-    `branch_id` BIGINT NULL,
-    PRIMARY KEY (`user_id`),
-    FOREIGN KEY (`branch_id`) REFERENCES `branch`(`branch_id`)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL
+CREATE TABLE `Doctor` (
+  `doctor_id` int,
+  `name` varchar(50),
+  `fee_per_patient` numeric(8,2),
+  `basic_monthly_salary` numeric(8,2),
+  `gender` varchar(6),
+  PRIMARY KEY (`doctor_id`)
 );
 
-
-CREATE Table `patient` (
-    `patient_id` BIGINT NOT NULL UNIQUE,
-    `name` VARCHAR(50) NOT NULL,
-    `gender` ENUM('Male','Female') NOT NULL,
-    `emergency_contact_no` VARCHAR(10) NOT NULL,
-    `nic` VARCHAR(12) NULL,
-    `address` VARCHAR(100) NULL,
-    `date_of_birth` DATE,
-    `blood_type` VARCHAR(5) NULL,    -- A+, A-, AB-, O+,....
-    PRIMARY KEY(`patient_id`),
-    Foreign Key (`patient_id`) REFERENCES `user`(`user_id`)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+CREATE TABLE `Doctor_Speciality` (
+  `doctor_id` int,
+  `specialiy_id` int,
+  `added_at` timestamp,
+  PRIMARY KEY (`doctor_id`, `specialiy_id`),
+  FOREIGN KEY (`specialiy_id`)
+      REFERENCES `Speciality`(`speciality_id`),
+  FOREIGN KEY (`doctor_id`)
+      REFERENCES `Doctor`(`doctor_id`)
 );
 
-
-CREATE TABLE `branch_manager` (
-    `manager_id` BIGINT NOT NULL UNIQUE,
-    `name` VARCHAR(50) NOT NULL,
-    `monthly_salary` DECIMAL(8,2) NOT NULL,
-    `gender` ENUM('Male','Female') NOT NULL,
-    PRIMARY KEY (`manager_id`),
-    FOREIGN KEY (`manager_id`) REFERENCES `user`(`user_id`)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+CREATE TABLE `Insurance` (
+  `insurance_id` int,
+  `insurance_type` varchar(20),
+  `insurance_period` varchar(20),
+  `claim_percentage` numeric(2,2),
+  `created_at` timestamp,
+  PRIMARY KEY (`insurance_id`)
 );
 
-
-CREATE TABLE `staff` (
-    `staff_id` BIGINT NOT NULL UNIQUE,
-    `name` VARCHAR(50) NOT NULL,
-    `type` ENUM(
-        'Admin_Staff',
-        'Nurse',
-        'Receptionist',
-        'Billing_Staff',
-        'Insurance_Agent'
-    ) NOT NULL,
-    `monthly_salary` DECIMAL(8,2) NOT NULL,
-    `gender` ENUM('Male','Female') NOT NULL,
-    PRIMARY KEY (`staff_id`),
-    FOREIGN KEY (`staff_id`) REFERENCES `user`(`user_id`)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+CREATE TABLE `Staff` (
+  `staff_id` int,
+  `name` varchar(50),
+  `type (receptionist, nurse, admin staff)` varchar(15),
+  `monthly_salary` numeric(8,2),
+  `gender` varchar(6),
+  PRIMARY KEY (`staff_id`)
 );
 
-
-CREATE TABLE `user_contact` (
-    `contact` VARCHAR(50) NOT NULL,
-    `contact_type` ENUM('Email','Phone_No') NOT NULL,
-    `is_default` BOOLEAN DEFAULT FALSE,
-    `user_id` BIGINT NOT NULL,
-    PRIMARY KEY (`contact`),
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+CREATE TABLE `Patient` (
+  `patient_id` int,
+  `name` varchar(50),
+  `gender` varchar(6),
+  `emergency_contact_no` varchar(10),
+  `nic` varchar(12),
+  `address` varchar(100),
+  `date_of_birth` date,
+  `blood_type` varchar(5),
+  PRIMARY KEY (`patient_id`)
 );
 
+CREATE TABLE `Treatment_Catelogue` (
+  `service_code` int,
+  `name` varchar(50),
+  `fee` numeric(8,2),
+  `description` varchar(255),
+  `speciality_id` int,
+  PRIMARY KEY (`service_code`),
+  FOREIGN KEY (`speciality_id`)
+      REFERENCES `Speciality`(`speciality_id`)
+);
 
+CREATE TABLE `Insurance_Claim` (
+  `claim_id` int,
+  `service_code` int,
+  `patient_id` int,
+  `approved_by` int,
+  `claimed_amount` numeric(8,2),
+  `claimed_at` timestamp,
+  `insurance_id` int,
+  PRIMARY KEY (`claim_id`),
+  FOREIGN KEY (`insurance_id`)
+      REFERENCES `Insurance`(`insurance_id`),
+  FOREIGN KEY (`approved_by`)
+      REFERENCES `Staff`(`staff_id`),
+  FOREIGN KEY (`patient_id`)
+      REFERENCES `Patient`(`patient_id`),
+  FOREIGN KEY (`service_code`)
+      REFERENCES `Treatment_Catelogue`(`service_code`)
+);
 
+CREATE TABLE `Branch` (
+  `branch_id` int,
+  `name` varchar(15),
+  `location` varchar(100),
+  `landline_no` varchar(10),
+  `created_at` timestamp,
+  PRIMARY KEY (`branch_id`)
+);
 
+CREATE TABLE `Billing_Invoice` (
+  `appointment_id` int,
+  `additional_fee` numeric(8,2),
+  `total_fee` numeric(8,2),
+  `claim_id` int,
+  `net_amount` numeric(8,2),
+  `remaining_payment_amount` numeric(8,2),
+  `time_stamp` timestamp,
+  PRIMARY KEY (`appointment_id`)
+);
+
+CREATE TABLE `Billing_Payment` (
+  `payment_id` int,
+  `invoice_id` int,
+  `branch_id` int,
+  `paid_amount` numeric(8,2),
+  `time_stamp` timestamp,
+  `cashier_id` int,
+  PRIMARY KEY (`payment_id`),
+  FOREIGN KEY (`branch_id`)
+      REFERENCES `Branch`(`branch_id`),
+  FOREIGN KEY (`invoice_id`)
+      REFERENCES `Billing_Invoice`(`appointment_id`),
+  FOREIGN KEY (`cashier_id`)
+      REFERENCES `Staff`(`staff_id`)
+);
+
+CREATE TABLE `Prescription` (
+  `appointment_id` int,
+  `consultation_note` varchar(255),
+  `prescription_items_details` Type,
+  `prescribed_at` timestamp,
+  `is_active` bool,
+  PRIMARY KEY (`appointment_id`)
+);
+
+CREATE TABLE `Action` (
+  `action_id` int,
+  `name` varchar(15),
+  PRIMARY KEY (`action_id`)
+);
+
+CREATE TABLE `Appointment` (
+  `appointment_id` int,
+  `patient_id` int,
+  `doctor_id` int,
+  `patient_note` varchar(255),
+  `date` date,
+  `time_slot` varchar(13),
+  `status (Scheduled / Completed / Cancelled)` varchar(10),
+  `time_stamp (for emergency walk-in)` timestamp,
+  PRIMARY KEY (`appointment_id`),
+  FOREIGN KEY (`patient_id`)
+      REFERENCES `Patient`(`patient_id`),
+  FOREIGN KEY (`doctor_id`)
+      REFERENCES `Doctor`(`doctor_id`)
+);
+
+CREATE TABLE `Medical_History` (
+  `medical_history_id` int,
+  `appointment_id` int,
+  `visit_date` date,
+  `diagnosis` varchar(255),
+  `symptoms` varchar(255),
+  `allergies` varchar(255),
+  `notes` varchar(255),
+  `follow_up_date` date,
+  `created_at` timestamp,
+  `updated_at` timestamp,
+  PRIMARY KEY (`medical_history_id`),
+  FOREIGN KEY (`appointment_id`)
+      REFERENCES `Appointment`(`appointment_id`)
+);
+
+CREATE TABLE `treatments` (
+  `service_code` int,
+  `appointment_id` int,
+  PRIMARY KEY (`service_code`, `appointment_id`),
+  FOREIGN KEY (`appointment_id`)
+      REFERENCES `Appointment`(`appointment_id`),
+  FOREIGN KEY (`service_code`)
+      REFERENCES `Treatment_Catelogue`(`service_code`)
+);
+
+CREATE TABLE `User` (
+  `user_id` int,
+  `username` varchar(20),
+  `password_hash` varchar(50),
+  `role (patient / doctor / staff / branch_manager)` varchar(15),
+  `branch_id` int,
+  `is_approved (to allow a user to login to the dashboard)` bool,
+  `created_at` timestamp,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`branch_id`)
+      REFERENCES `Branch`(`branch_id`)
+);
+
+CREATE TABLE `User_Contact` (
+  `contact ` varchar(50),
+  `contact_type (email or phone_no)` varchar(8),
+  `is_default` bool,
+  `user_id` int,
+  PRIMARY KEY (`contact `),
+  FOREIGN KEY (`user_id`)
+      REFERENCES `User`(`user_id`)
+);
+
+CREATE TABLE `Branch_Manager` (
+  `manager_id` int,
+  `name` varchar(50),
+  `monthly_salary` numeric(8,2),
+  `gender` varchar(6),
+  PRIMARY KEY (`manager_id`)
+);
+
+CREATE TABLE `Log` (
+  `log_id` int,
+  `user_id` int,
+  `user_role` varchar(15),
+  `action_id` int,
+  `table_name` varchar(15),
+  `record_id (PK in updated table)` int,
+  `time_Stamp` timestamp,
+  `details` varchar(255),
+  PRIMARY KEY (`log_id`),
+  FOREIGN KEY (`action_id`)
+      REFERENCES `Action`(`action_id`),
+  FOREIGN KEY (`user_id`)
+      REFERENCES `User`(`user_id`)
+);
+
+CREATE TABLE `Patient_Insurance` (
+  `patient_id` int,
+  `insurance_id` int,
+  `created_at` timestamp,
+  `is_expired` bool,
+  PRIMARY KEY (`patient_id`, `insurance_id`),
+  FOREIGN KEY (`patient_id`)
+      REFERENCES `Patient`(`patient_id`),
+  FOREIGN KEY (`insurance_id`)
+      REFERENCES `Insurance`(`insurance_id`)
+);
 
