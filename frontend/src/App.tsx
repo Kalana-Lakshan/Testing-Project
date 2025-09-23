c5cc20e78f4333c7d3f85ae77a3f932eb827afe8
KRakeesh04 <kanesrakeshan@gmail.com>
2025-09-24 02:35:38 +0530

    chore: add dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,75 +3,82 @@ import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from 'react-router-dom';
 import PageTitle from './components/PageTitle';
 import DefaultLayout from './DefaultLayout';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/dashboard';
 import { Skeleton } from "./components/ui/skeleton";
-import SignIn from "./pages/Authentication/sign-in";  
-    
+import SignIn from "./pages/Authentication/sign-in";
+import SignUp from './pages/Authentication/sign-up';
+import Users from './pages/users/users';
+import { ThemeProvider } from './components/theme-provider';
+
 
 function App() {
   const [loading, setLoading] = useState<boolean>(false);
-	const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-		window.scrollTo(0, 0);
-	}, [pathname]);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return loading ? (
-		<div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-        <Skeleton className="h-4 w-[150px]" />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[200px] w-[500px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[500px]" />
+          <Skeleton className="h-4 w-[300px]" />
+          <Skeleton className="h-4 w-[150px]" />
+        </div>
       </div>
-    </div>
-	) : (
-    <>
-      <Routes>
+    </ThemeProvider>
+  ) : (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <Routes>
+          <Route
+            path="/sign-in"
+            element={
+              <>
+                <PageTitle title="Sign-in | MedSync" />
+                <SignIn />
+              </>
+            }
+          />
+
+          <Route
+            path="/sign-up"
+            element={
+              <>
+                <PageTitle title="Sign-up | MedSync" />
+                <SignUp />
+              </>
+            }
+          />
+
+          <Route element={<DefaultLayout />}>
             <Route
-              path="/sign-in"
+              index
               element={
                 <>
-                  <PageTitle title="Sign-in | MedSync" />
-                  <SignIn />
+                  <PageTitle title="Dashboard | MedSync" />
+                  <Dashboard />
                 </>
               }
             />
 
             <Route
-              path="/sign-up"
+              path="/users"
               element={
                 <>
-                  <PageTitle title="Sign-up | MedSync" />
-                  {/* <SignUp /> */}
+                  <PageTitle title="Users | MedSync" />
+                  <Users />
                 </>
               }
             />
 
-            <Route element={<DefaultLayout />}>
-              <Route
-                index
-                element={
-                  <>
-                    <PageTitle title="Dashboard | MedSync" />
-                    <Dashboard />
-                  </>
-                }
-              />
-
-              <Route
-              path=""
-              element={
-                <>
-                  {/* <PageTitle title="" /> */}
-                  {/* < /> */}
-                </>
-              }
-            />
-
-            </Route>
-      </Routes>
-    </>
+          </Route>
+        </Routes>
+      </>
+    </ThemeProvider>
   );
 }
 
