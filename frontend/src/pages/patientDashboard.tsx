f26644bac88257b6d3abd149eb678401d51ec528
KRakeesh04 <kanesrakeshan@gmail.com>
2025-10-02 01:10:00 +0530

    chore: add dashboard redirect

---
+++ frontend/src/pages/patientDashboard.tsx
@@ -0,0 +1,11 @@
+const PatientDashboard: React.FC = () => {
+  return (
+    <>
+      <h1>
+        hiii this is patient Dashboard
+      </h1>
+    </>
+  );
+};
+
+export default PatientDashboard;
