e69ad93d1cd4d639963c8418db5c50d372a47392
KRakeesh04 <kanesrakeshan@gmail.com>
2025-10-02 01:18:49 +0530

    chore: update signin  service function

--- frontend/src/services/authServices.tsx
+++ frontend/src/services/authServices.tsx
@@ -7,20 +7,62 @@ export const LOCAL_STORAGE__USERNAME = "username";
 export const LOCAL_STORAGE__ROLE = "role";
 export const LOCAL_STORAGE__USER_ID = "user_id";
 
+export interface StaffData {
+  username: string
+  email: string
+  password: string
+  phoneNo: string
+  gender: string
+  role: string
+  branch: number
+}
+
+export interface PatientData {
+  fullname: string
+  nic: string
+  username: string
+  email: string
+  password: string
+  phoneNo: string
+  emergencyContactNo: string
+  DOB: string
+  gender: string
+  bloodType: string
+  branch: number
+  address: string
+}
+
 export const signin = async (username: string, password: string) => {
   try {
     const response = await axiosInstance.post("/auth/sign-in", {
       username,
       password,
-    });
-    const token = response.data.token;
-    const user = response.data.user;
-    localStorage.setItem(LOCAL_STORAGE__TOKEN, token);
-    localStorage.setItem(LOCAL_STORAGE__USER, JSON.stringify(user));
-    localStorage.setItem(LOCAL_STORAGE__USERNAME, user.username);
-    localStorage.setItem(LOCAL_STORAGE__ROLE, user.role);
-    localStorage.setItem(LOCAL_STORAGE__USER_ID, user.id);
-    return true;
+    })
+    const token = response.data.token
+    const user = response.data.user
+    localStorage.setItem(LOCAL_STORAGE__TOKEN, token)
+    localStorage.setItem(LOCAL_STORAGE__USER, JSON.stringify(user))
+    localStorage.setItem(LOCAL_STORAGE__USERNAME, user.username)
+    localStorage.setItem(LOCAL_STORAGE__ROLE, user.role)
+    localStorage.setItem(LOCAL_STORAGE__USER_ID, user.id)
+    return true
+  } catch (error: unknown) {
+    console.error("Error logging in:", error)
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.error) {
+        throw error.response.data.error
+      }
+      throw error.message
+    }
+    throw "Unknown error occurred"
+  }
+};
+
+export const staffSignup = async (data: StaffData) => {
+  try {
+    const response = await axiosInstance.post("/auth/sign-up/staff", data)
+    const message = response.data.message
+    return message
   } catch (error: unknown) {
     console.error("Error logging in:", error);
     if (error instanceof AxiosError) {
