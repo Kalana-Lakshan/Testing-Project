8dd3994d8012aa8281765e6a42d5459ec1f8ee91
KRakeesh04 <kanesrakeshan@gmail.com>
2025-10-02 11:34:13 +0530

    chore: add userService functions for edit delete and restore actions

--- frontend/src/services/userService.tsx
+++ frontend/src/services/userService.tsx
@@ -29,7 +29,7 @@ export const getAllUsers = async (count: number, offset: number) => {
     }>(`/users/active?count=${count}&offset=${offset}`);
     return users_db.data;
   } catch (error: unknown) {
-    console.error("Error logging in:", error);
+    console.error("Error getting all users:", error);
     if (error instanceof AxiosError) {
       if (error.response?.data?.error) {
         throw error.response.data.error;
@@ -39,3 +39,72 @@ export const getAllUsers = async (count: number, offset: number) => {
     throw "Unknown error occurred";
   }
 };
+
+export const getAllInactiveUsers = async (count: number, offset: number) => {
+  try {
+    const users_db = await axiosInstance.get<{
+      user_count: number;
+      users: Array<User>;
+    }>(`/users/inactive?count=${count}&offset=${offset}`);
+    return users_db.data;
+  } catch (error: unknown) {
+    console.error("Error getting all deleted users:", error);
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.error) {
+        throw error.response.data.error;
+      }
+      throw error.message;
+    }
+    throw "Unknown error occurred";
+  }
+};
+
+export const editUser = async (data: any) => {
+  try {
+    const response = await axiosInstance.put(`/user/edit/${data.user_id}`,
+      data
+    )
+    return response.data
+  } catch (error: unknown) {
+    console.error("Error editing user:", error);
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.error) {
+        throw error.response.data.error;
+      }
+      throw error.message;
+    }
+    throw "Unknown error occurred";
+  }
+}
+
+export const deleteUser = async (user_id: number) => {
+  try {
+    const response = await axiosInstance.delete(`/user/delete/${user_id}`)
+    return response.data
+  } catch (error: unknown) {
+    console.error("Error deleting user:", error);
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.error) {
+        throw error.response.data.error;
+      }
+      throw error.message;
+    }
+    throw "Unknown error occurred";
+  }
+}
+
+export const restoreDeletedUser = async (user_id: number) => {
+  try {
+    const response = await axiosInstance.put(`/user/restore/${user_id}`)
+    return response.data
+  } catch (error: unknown) {
+    console.error("Error restoring user:", error);
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.error) {
+        throw error.response.data.error;
+      }
+      throw error.message;
+    }
+    throw "Unknown error occurred";
+  }
+}
