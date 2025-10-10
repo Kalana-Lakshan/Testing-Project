23b687633c549bc6e69ebdb53085d35d6282e6ba
KRakeesh04 <kanesrakeshan@gmail.com>
2025-10-10 12:17:42 +0530

    chore: rename the router in frontend for all-branches

--- backend/src/handlers/branch.handler.ts
+++ backend/src/handlers/branch.handler.ts
@@ -1,6 +1,12 @@
 import type { Request, Response } from "express";
 import { createBranch, getAllBranches, getBranchCount, getBranchesForPagination, updateBranch, type Branch } from "../models/branch.model.ts";
 
+
+interface BranchNames {
+  branch_id: number,
+  name: string,
+}
+
 export const createNewBranch = async (req: Request, res: Response) => {
   let { name, location, landline_no } = req.body;
   try {
@@ -18,12 +24,10 @@ export const createNewBranch = async (req: Request, res: Response) => {
 export const getBranches = async (req: Request, res: Response) => {
   const { count, offset } = req.query;
   try {
-    let branches: Branch[];
     if (!count || !offset) {
-      branches = await getAllBranches();
-    } else {
-      branches = await getBranchesForPagination(Number(count), Number(offset));
+      res.status(400).json({ error: "Params count & offset undefined" });
     }
+    const branches: Branch[] = await getBranchesForPagination(Number(count), Number(offset));
     if (branches.length < 1) {
       res.status(404).json({ error: "Branches not found" });
     }
@@ -36,6 +40,33 @@ export const getBranches = async (req: Request, res: Response) => {
     res.status(200).json({
       branch_count: branch_count,
       branches: branches
+    });
+  } catch (error) {
+    console.log("error in getBranches handler: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  };
+};
+
+export const getAllBranchNames = async (req: Request, res: Response) => {
+  try {
+    const branches: Branch[] = await getAllBranches();
+    if (branches.length < 1) {
+      res.status(404).json({ error: "Branches not found" });
+      return;
+    }
+    const branch_count: Number = await getBranchCount();
+    if (branch_count == undefined) {
+      console.log("error in finding the branch count, count = " + branch_count);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    const mappedBranches: BranchNames[] = branches.map((b) => ({
+      branch_id: b.branch_id,
+      name: b.name,
+    }));
+    res.status(200).json({
+      branch_count: branch_count,
+      branches: mappedBranches
     });
   } catch (error) {
     console.log("error in getBranches handler: ", error);
